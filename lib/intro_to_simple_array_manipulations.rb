53980e54d830676d6c52453992061aae0e55cb2a
Vincent Martyr <vincentmartyr@gmail.com>
2020-02-06 01:57:24 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -56,7 +56,7 @@ end
 def using_delete(array, string)
   instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
   string = "Steven"
-  array = instructors.delete("Steven")
+  instructors.delete("Steven")
 end
 
 def using_delete_at(array, interger)
