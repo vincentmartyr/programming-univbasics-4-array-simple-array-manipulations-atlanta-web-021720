b43830c0a8b4fb0100744c38d519b17a227e56d2
Vincent Martyr <vincentmartyr@gmail.com>
2020-02-06 02:48:01 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -12,8 +12,8 @@ def using_unshift(array, string)
 end
 
 def using_pop(array)
-  continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
-  continents.pop
+  #continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+  array.pop
 end
 
 
