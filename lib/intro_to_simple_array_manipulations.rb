9344b14bac0e38e1ad60a31a2b8e22bb22b98bd0
Vincent Martyr <vincentmartyr@gmail.com>
2020-02-06 02:54:43 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,7 +1,7 @@
 def using_push(array, string)
-  colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
-  string = "violet"
-  updated_array = colors_in_the_rainbow.push(string)
+  #colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+  #string = "violet"
+  array.push(string)
 end
 
 
