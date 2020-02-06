987249fe7924cfc2c04e94a6efd945ae83ed9e51
Vincent Martyr <vincentmartyr@gmail.com>
2020-02-06 02:50:14 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -23,8 +23,8 @@ def pop_with_args(array)
 end
 
 def using_shift(array)
-  favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
-  array = favorite_cities.shift
+  #favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
+  array.shift
 end
 
 def shift_with_args(array)
